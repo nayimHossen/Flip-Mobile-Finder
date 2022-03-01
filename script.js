bfef1d3ba093091339457451456c9c82a6ad3a46
Nayim hossen <nayimh2015@gmail.com>
2022-03-01 20:05:59 +0600

    design

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const displayPhone = (phones) => {
             div.classList.add('col');
 
             div.innerHTML = `
-            <div id="phone" class="card h-100">
+            <div id="phone" class="card h-100 shadow-sm">
                 <img src="${phone.image}" class="w-50 mx-auto pt-3" alt="...">
                 <div class="card-body row align-item-center justify-content-center">
                     <div class="col-7">
@@ -41,7 +41,7 @@ const displayPhone = (phones) => {
                         <p class="card-text">${phone.brand}</p>
                     </div>
                     <div class="col-5">
-                        <button onclick="showDetails('${phone.slug}')" class="btn btn-primary px-4 py-2">Details</button>
+                        <button onclick="showDetails('${phone.slug}')" class="btn searchButton px-4 py-2">Details</button>
                     </div>
                 </div>
             </div>
